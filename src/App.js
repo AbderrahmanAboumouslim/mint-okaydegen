326ce6e1a2882e31b0716c26d2f205314880c92a
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-05-14 16:09:52 +0100

    testing code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <div>Test</div>
+      <div id="mint-counter"></div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: pink;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
